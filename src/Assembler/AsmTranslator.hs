88a66ffc5821943c7b05fafbf3c7ddd12149a886
Boris Nikulin <NikulinBE@gmail.com>
2018-01-27 19:03:59 -0800

    move from String to ByteString for translation

--- src/Assembler/AsmTranslator.hs
+++ src/Assembler/AsmTranslator.hs
@@ -8,20 +8,24 @@ import Text.AsmParser (SymbolTable)
 import Numeric (showIntAtBase)
 import Data.Char (intToDigit)
 import Data.List (replicate)
+import Data.Semigroup
+import qualified Data.ByteString.Char8 as B
+import qualified Data.ByteString.Lazy.Char8 as BL
+import Data.ByteString.Builder
 
-tReg :: Reg -> String
-tReg (Reg r) = replicate padAmmount '0'
-	++ showIntAtBase 2 intToDigit r ""
+tReg :: Reg -> Builder
+tReg (Reg r) = byteString (B.replicate padAmmount '0')
+	<> string8 (showIntAtBase 2 intToDigit r "")
 	where
-		str = showIntAtBase 2 intToDigit r ""
 		padAmmount = if
 			| r <= 1    -> 2
 			| r <= 4    -> 1
 			| otherwise -> 0
 
+tlb = toLazyByteString
 
-translateAsm :: SymbolTable -> Inst -> String
-translateAsm _ (Mv  r) = "000000" ++ tReg r
-translateAsm _ (Str r) = "000010" ++ tReg r
-translateAsm _ (Ld  r) = "000011" ++ tReg r
-translateAsm _ (Fin)   = "000111000"
+translateAsm :: SymbolTable -> (Word, Inst) -> BL.ByteString
+translateAsm _ (_, (Mv  r)) = tlb $ "000000" <> tReg r
+translateAsm _ (_, (Str r)) = tlb $ "000010" <> tReg r
+translateAsm _ (_, (Ld  r)) = tlb $ "000011" <> tReg r
+translateAsm _ (_, (Fin))   = tlb $ "000111000"
