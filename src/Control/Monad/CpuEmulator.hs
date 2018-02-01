7e0f4781bbe8d6b1485b6445b3a6b856e0cafa63
Boris Nikulin <NikulinBE@gmail.com>
2018-02-01 03:46:32 -0800

    CpuEmulator made instance of Monad and supporting classes

--- src/Control/Monad/CpuEmulator.hs
+++ src/Control/Monad/CpuEmulator.hs
@@ -30,16 +30,34 @@ data CpuState w = CpuState
 
 newtype Cpu w s a = Cpu { unCpu :: ReaderT (CpuEnv w s) (ST s) a }
 
-freezeCpuEnv :: PrimMonad m => CpuEnv w (PrimState m) ->  m (CpuState w)
-freezeCpuEnv (CpuEnv regs flags ram) = CpuState <$> V.freeze regs <*> V.freeze flags <*> V.freeze ram
+unsafeFreezeCpuEnv :: PrimMonad m => CpuEnv w (PrimState m) ->  m (CpuState w)
+unsafeFreezeCpuEnv (CpuEnv regs flags ram) =
+	CpuState
+	<$> V.unsafeFreeze regs
+	<*> V.unsafeFreeze flags
+	<*> V.unsafeFreeze ram
 
 runCpu :: forall a w. (forall s. Cpu w s a) -> (forall s. CpuEnv w s) -> (a, CpuState w)
 runCpu cpu env = runST $ runReaderT readerComp env
 	where
 		readerComp :: ReaderT (CpuEnv w s) (ST s) (a, CpuState w)
-		readerComp =
-			unCpu cpu  >>= \a -> do
-				frozenEnv <- ReaderT freezeCpuEnv
-				return (a, frozenEnv)
+		readerComp = do
+			a <- unCpu cpu
+			frozenEnv <- ReaderT unsafeFreezeCpuEnv
+			return (a, frozenEnv)
 
+instance Functor (Cpu w s) where
+	fmap f (Cpu (ReaderT r)) = Cpu . ReaderT $ \env -> fmap f (r env)
 
+instance Applicative (Cpu w s) where
+	pure = Cpu . lift . pure
+	(Cpu f) <*> (Cpu r) = Cpu (f <*> r)
+
+instance Monad (Cpu w s) where
+	(Cpu r) >>= f = Cpu . ReaderT $ \env -> do
+		a <- runReaderT r env
+		let (Cpu r2) = f a
+		runReaderT r2 env
+
+getReg :: CpuEnv w s -> Int -> Cpu w s w
+getReg (CpuEnv regs _ _) i = Cpu $ MV.read regs i
