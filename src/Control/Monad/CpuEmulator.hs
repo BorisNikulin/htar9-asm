e79c01e33ac51ad481435f8cde6a267b1d19200c
Boris Nikulin <NikulinBE@gmail.com>
2018-02-01 20:23:59 -0800

    mtl support for the Cpu monad

--- src/Control/Monad/CpuEmulator.hs
+++ src/Control/Monad/CpuEmulator.hs
@@ -1,6 +1,12 @@
 {-# LANGUAGE TemplateHaskell
 	, RankNTypes
 	, ScopedTypeVariables
+	, DefaultSignatures
+	, MultiParamTypeClasses
+	, FunctionalDependencies
+	, TypeFamilies
+	, FlexibleInstances
+	, UndecidableInstances
 	#-}
 
 module Control.Monad.CpuEmulator where
@@ -12,6 +18,11 @@ import Lens.Micro.Platform
 import Control.Monad.Primitive
 import Control.Monad.Reader
 import Control.Monad.ST
+import Control.Monad.Writer
+import Control.Monad.State
+import Control.Monad.RWS
+import Control.Monad.Except
+import Control.Monad.Cont
 
 data CpuEnv w s = CpuEnv
 	{ _regs  :: MV.MVector s w
@@ -68,39 +79,65 @@ instance Monad (Cpu w s) where
 		let (Cpu r2) = f a
 		runReaderT r2 env
 
-getReg :: Int -> Cpu w s w
-getReg i = Cpu . ReaderT $ \r -> MV.read (r^.regs) i
+-- mtl typeclasses
+class Monad m => MonadCpu w m | m -> w  where
+	getReg :: Int -> m ()
+	default getReg :: (MonadTrans t, MonadCpu w m1, m ~ t m1) => Int -> m ()
+	getReg = lift . getReg
 
-setReg :: Int -> w -> Cpu w s ()
-setReg i x = Cpu . ReaderT $ \r -> MV.write (r^.regs) i x
+	setReg :: Int -> w -> m ()
+	default setReg :: (MonadTrans t, MonadCpu w m1, m ~ t m1) => Int -> w -> m ()
+	setReg = (lift .) . setReg
 
-getFlag :: Int -> Cpu w s Bool
-getFlag i = Cpu . ReaderT $ \r -> MV.read (r^.flags) i
+	getFlag :: Int -> m Bool
+	default getFlag :: (MonadTrans t, MonadCpu w m1, m ~ t m1) => Int -> m Bool
+	getFlag = lift . getFlag
 
-setFlag :: Int -> Bool -> Cpu w s ()
-setFlag i b = Cpu . ReaderT $ \r -> MV.write (r^.flags) i b
+	setFlag :: Int -> Bool -> m ()
+	default setFlag :: (MonadTrans t, MonadCpu w m1, m ~ t m1) => Int -> Bool -> m ()
+	setFlag = (lift .) . setFlag
 
-getRam :: Int -> Cpu w s w
-getRam i = Cpu . ReaderT $ \r -> MV.read (r^.ram) i
+	getRam :: Int -> m ()
+	default getRam :: (MonadTrans t, MonadCpu w m1, m ~ t m1) => Int -> m ()
+	getRam = lift . getRam
 
-setRam :: Int -> w -> Cpu w s ()
-setRam i x = Cpu . ReaderT $ \r -> MV.write (r^.ram) i x
+	setRam :: Int -> w -> m ()
+	default setRam :: (MonadTrans t, MonadCpu w m1, m ~ t m1) => Int -> w -> m ()
+	setRam = (lift .) . setRam
 
-v = V.fromList [0, 1, 2, 3, 20] :: V.Vector Int
-b = V.fromList [False] :: V.Vector Bool
-state = CpuState v b v
+instance MonadCpu w (Cpu w s) where
+	getReg i = Cpu $ ReaderT (\r -> MV.read (r^.regs) i) >> return ()
+	setReg i x = Cpu . ReaderT $ \r -> MV.write (r^.regs) i x
 
-test = runCpu comp state
+	getFlag i = Cpu . ReaderT $ \r -> MV.read (r^.flags) i
+	setFlag i b = Cpu . ReaderT $ \r -> MV.write (r^.flags) i b
 
-comp = do
-	r0 <- getReg 0
-	r4 <- getReg 4
-	{-return $ r0 + r4-}
-	v <- getRam 3
-	setFlag 0 True
-	flag <- getFlag 0
-	setReg 0 100
-	if flag
-		then return r0
-		else return r4
+	getRam i = Cpu $ ReaderT (\r -> MV.read (r^.ram) i) >> return ()
+	setRam i x = Cpu . ReaderT $ \r -> MV.write (r^.ram) i x
+
+
+instance MonadCpu w m => MonadCpu w (ReaderT r m)
+instance (MonadCpu w m, Monoid l) => MonadCpu w (WriterT l m)
+instance MonadCpu w m => MonadCpu w (StateT s m)
+instance (MonadCpu w m, Monoid l) => MonadCpu w (RWST r l s m)
+instance MonadCpu w m => MonadCpu w (ExceptT e m)
+instance MonadCpu w m => MonadCpu w (ContT r m)
+
+{-v = V.fromList [0, 1, 2, 3, 20] :: V.Vector Int-}
+{-b = V.fromList [False] :: V.Vector Bool-}
+{-state = CpuState v b v-}
+
+{-test = runCpu comp state-}
+
+{-comp = do-}
+	{-r0 <- getReg 0-}
+	{-r4 <- getReg 4-}
+	{-{-return $ r0 + r4-}-}
+	{-v <- getRam 3-}
+	{-setFlag 0 True-}
+	{-flag <- getFlag 0-}
+	{-setReg 0 100-}
+	{-if flag-}
+		{-then return r0-}
+		{-else return r4-}
 
