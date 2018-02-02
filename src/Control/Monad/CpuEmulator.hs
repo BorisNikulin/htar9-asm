0bf5535a490d8748dfcdb846579220a537c5c50d
Boris Nikulin <NikulinBE@gmail.com>
2018-02-01 21:05:28 -0800

    changes to MonadCpu and other small changes

--- src/Control/Monad/CpuEmulator.hs
+++ src/Control/Monad/CpuEmulator.hs
@@ -38,8 +38,14 @@ data CpuState w = CpuState
 	, cpuRam   :: V.Vector w
 	} deriving (Show)
 
-
 newtype Cpu w s a = Cpu { unCpu :: ReaderT (CpuEnv w s) (ST s) a }
+
+thawCpuState :: PrimMonad m => CpuState w -> m (CpuEnv w (PrimState m))
+thawCpuState (CpuState regs flags ram) =
+	CpuEnv
+	<$> V.thaw regs
+	<*> V.thaw flags
+	<*> V.thaw ram
 
 unsafeThawCpuState :: PrimMonad m => CpuState w -> m (CpuEnv w (PrimState m))
 unsafeThawCpuState (CpuState regs flags ram) =
@@ -55,9 +61,9 @@ unsafeFreezeCpuEnv (CpuEnv regs flags ram) =
 	<*> V.unsafeFreeze flags
 	<*> V.unsafeFreeze ram
 
-runCpu :: forall a w. (forall s. Cpu w s a) -> CpuState w -> (a, CpuState w)
-runCpu cpu state = runST $ do
-	cpuEnv <- unsafeThawCpuState state
+runCpu' :: forall a w. (forall s. CpuState w -> ST s (CpuEnv w s)) -> (forall s. Cpu w s a) -> CpuState w -> (a, CpuState w)
+runCpu' thaw cpu state = runST $ do
+	cpuEnv <- thaw state
 	runReaderT readerComp cpuEnv
 	where
 		readerComp :: ReaderT (CpuEnv w s) (ST s) (a, CpuState w)
@@ -65,6 +71,16 @@ runCpu cpu state = runST $ do
 			a <- unCpu cpu
 			frozenEnv <- ReaderT unsafeFreezeCpuEnv
 			return (a, frozenEnv)
+
+-- | Runs Cpu by safely thawing provided vectors O(n).
+-- The provided vectors can still be used after the function.
+runCpu :: (forall s. Cpu w s a) -> CpuState w -> (a, CpuState w)
+runCpu = runCpu' thawCpuState
+
+-- | Runs Cpu by unsafely thawing provided vectors O(1).
+-- The provided vectors must not be used after this function.
+unsafeRunCpu :: (forall s. Cpu w s a) -> CpuState w -> (a, CpuState w)
+unsafeRunCpu = runCpu' unsafeThawCpuState
 
 instance Functor (Cpu w s) where
 	fmap f (Cpu (ReaderT r)) = Cpu . ReaderT $ \env -> fmap f (r env)
@@ -81,8 +97,8 @@ instance Monad (Cpu w s) where
 
 -- mtl typeclasses
 class Monad m => MonadCpu w m | m -> w  where
-	getReg :: Int -> m ()
-	default getReg :: (MonadTrans t, MonadCpu w m1, m ~ t m1) => Int -> m ()
+	getReg :: Int -> m w
+	default getReg :: (MonadTrans t, MonadCpu w m1, m ~ t m1) => Int -> m w
 	getReg = lift . getReg
 
 	setReg :: Int -> w -> m ()
@@ -97,8 +113,8 @@ class Monad m => MonadCpu w m | m -> w  where
 	default setFlag :: (MonadTrans t, MonadCpu w m1, m ~ t m1) => Int -> Bool -> m ()
 	setFlag = (lift .) . setFlag
 
-	getRam :: Int -> m ()
-	default getRam :: (MonadTrans t, MonadCpu w m1, m ~ t m1) => Int -> m ()
+	getRam :: Int -> m w
+	default getRam :: (MonadTrans t, MonadCpu w m1, m ~ t m1) => Int -> m w
 	getRam = lift . getRam
 
 	setRam :: Int -> w -> m ()
@@ -106,13 +122,13 @@ class Monad m => MonadCpu w m | m -> w  where
 	setRam = (lift .) . setRam
 
 instance MonadCpu w (Cpu w s) where
-	getReg i = Cpu $ ReaderT (\r -> MV.read (r^.regs) i) >> return ()
+	getReg i = Cpu . ReaderT $ \r -> MV.read (r^.regs) i
 	setReg i x = Cpu . ReaderT $ \r -> MV.write (r^.regs) i x
 
 	getFlag i = Cpu . ReaderT $ \r -> MV.read (r^.flags) i
 	setFlag i b = Cpu . ReaderT $ \r -> MV.write (r^.flags) i b
 
-	getRam i = Cpu $ ReaderT (\r -> MV.read (r^.ram) i) >> return ()
+	getRam i = Cpu . ReaderT $ \r -> MV.read (r^.ram) i
 	setRam i x = Cpu . ReaderT $ \r -> MV.write (r^.ram) i x
 
 
@@ -123,20 +139,23 @@ instance (MonadCpu w m, Monoid l) => MonadCpu w (RWST r l s m)
 instance MonadCpu w m => MonadCpu w (ExceptT e m)
 instance MonadCpu w m => MonadCpu w (ContT r m)
 
-{-v = V.fromList [0, 1, 2, 3, 20] :: V.Vector Int-}
-{-b = V.fromList [False] :: V.Vector Bool-}
-{-state = CpuState v b v-}
+v = V.fromList [0, 1, 2, 3, 20] :: V.Vector Int
+b = V.fromList [False] :: V.Vector Bool
+initState = CpuState v b v
 
-{-test = runCpu comp state-}
+test = runCpu (runReaderT comp 2) initState
 
-{-comp = do-}
-	{-r0 <- getReg 0-}
+comp :: ReaderT Int (Cpu Int s) Int
+comp = do
+	env <- ask
+	setReg 0 env
+	r0 <- getReg 0
+	return r0
 	{-r4 <- getReg 4-}
 	{-{-return $ r0 + r4-}-}
 	{-v <- getRam 3-}
 	{-setFlag 0 True-}
 	{-flag <- getFlag 0-}
-	{-setReg 0 100-}
 	{-if flag-}
 		{-then return r0-}
 		{-else return r4-}
