18b8737b38dabf0ecf48e65e94041007283ccd02
Boris Nikulin <NikulinBE@gmail.com>
2018-02-05 16:43:27 -0800

    argument parser and haskell executable that can assemble htar9 asm

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,40 @@
-module Main where
+{-# LANGUAGE RecordWildCards #-}
+
+module Main
+	( main
+	) where
+
+import Text.ArgParser
+import Text.AsmParser
+import Text.AsmTranslator
+
+import System.IO
+import Data.Semigroup
+import qualified Data.ByteString.Lazy as BL
+import Text.Megaparsec.Error (parseErrorPretty)
 
 main :: IO ()
-main = putStrLn "Hello, World!"
+main = do
+	opts <- parseArgs
+	case opts of
+		AssembleOptions{..}  ->
+			-- try catch here too
+			withFile optInputFile ReadMode $
+				assembleFile opts
+		InterpretOptions{} -> return ()
+	return ()
+
+assembleFile :: Options -> Handle -> IO ()
+assembleFile AssembleOptions{..} h = do
+	input <- hGetContents h
+	case parseAsm optInputFile input of
+		Right (insts, table) ->
+			case translateAsms table insts of
+				-- probably need a try catch here
+				Right outputList -> BL.writeFile optOutputFile output
+					where
+						output = if optFormatted
+							then BL.intercalate "\n" outputList <> "\n"
+							else BL.concat outputList
+				Left te -> putStrLn (labelErrorPretty te)
+		Left pe -> putStrLn (parseErrorPretty pe)
---
+++ src/Text/ArgParser.hs
@@ -0,0 +1,41 @@
+module Text.ArgParser where
+
+import Data.Semigroup
+import Options.Applicative
+
+data Options =
+	AssembleOptions
+	{ optFormatted  :: Bool
+	, optOutputFile :: FilePath
+	, optInputFile  :: FilePath
+	}
+	| InterpretOptions
+	{ optInputFile  :: FilePath
+	} deriving (Show)
+
+assembleArgParser :: Parser Options
+assembleArgParser =
+	AssembleOptions
+	<$> switch
+		(  long "formatted"
+		<> short 'f'
+		<> help "<help>")
+	<*> strOption
+		(  long "output"
+		<> short 'o'
+		<> metavar "OUTPUT_FILE"
+		<> help "<help>")
+	<*> strArgument
+		(  metavar "INPUT_FILE"
+		<> help "<help>")
+
+argParser :: Parser Options
+argParser = assembleArgParser
+
+parseArgs :: IO Options
+parseArgs = execParser opts
+	where
+		opts = info (argParser <**> helper)
+			(  fullDesc
+			<> header "htar9-asm-hs-exe"
+			<> progDesc "HTAR9 Assembler and Interpreter")
